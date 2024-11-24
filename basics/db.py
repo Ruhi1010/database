89ada0e8a3adde642049fe9ffd9dade81b0f6309
Ruhi1010 <rashedial52@gmail.com>
2024-11-24 22:23:10 +0600

    .

--- basics/db.py
+++ basics/db.py
@@ -6,6 +6,11 @@ cur = con.cursor()
 # cur.execute("CREATE TABLE movie(title, year, score)")
 # cur.execute("CREATE TABLE students(student_id, student_name, student_phone)")
 
+cur.execute("INSERT INTO movie VALUES ('PRISONERS', '2000', '9')")
+
 res = cur.execute("SELECT * FROM movie")
+
+con.commit()
+
 
 print(res.fetchall())
